f9947a1da40adb9e34d4b997ba5f4094d390fc3e
shweta shetye <shweta.shetye@protegrity.com>
2020-09-02 21:18:02 +0530

    Updated side bar to pink

--- source/conf.py
+++ source/conf.py
@@ -48,6 +48,10 @@ master_doc = 'index'
 #
 html_theme = "sphinx_rtd_theme"
 html_theme_path = ["_themes", ]
+html_theme_options = {
+    "rightsidebar": "true",
+    "relbarbgcolor": "pink"
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
