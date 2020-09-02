55f1b0f0c2f3dfeae4db8008ba861fe8d80909bd
shweta shetye <shweta.shetye@protegrity.com>
2020-09-02 20:50:08 +0530

    Updating the theme folder.

--- source/conf.py
+++ source/conf.py
@@ -47,8 +47,7 @@ master_doc = 'index'
 # a list of builtin themes.
 #
 html_theme = "sphinx_rtd_theme"
-#html_theme_path = ['_themes']
-#html_theme = 'kr'
+html_theme_path = ["_themes", ]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
